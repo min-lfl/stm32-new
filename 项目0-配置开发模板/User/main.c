44122020f824c4cdab819d42d20e2eee5f6caab0
min-lfl <2775381313@qq.com>
2025-11-26 20:17:26 +0800

    更新了开发模板，增加了私有硬件驱动库以及一些私有硬件

--- 项目0-配置开发模板/User/main.c
+++ 项目0-配置开发模板/User/main.c
@@ -1,12 +1,13 @@
 #include "stm32f10x.h"                  // Device header
 #include <stdio.h>
 #include <DELAY.H>
+#include <LED.H>
+#include <KEY.H>
 
-struct{char aaa;int bbb;float ccc;}Abc;
 
 int main(void){
-
+	
 	while(1){
-		
+	
 	}
 }
