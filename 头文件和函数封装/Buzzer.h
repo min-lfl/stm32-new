b2ef0f82f47a97b4560a31a14ff6097a5c777296
min-lfl <2775381313@qq.com>
2025-11-26 21:12:39 +0800

    完成了光敏电阻控制蜂鸣器项目

---
+++ 头文件和函数封装/Buzzer.h
@@ -0,0 +1,9 @@
+#ifndef __Buzzer_H__
+#define __Buzzer_H__
+
+void Buzzer_Init(void);  	//初始化蜂鸣器
+void Buzzer_ON(void);		//打开蜂鸣器
+void Buzzer_OFF(void);		//关闭蜂鸣器
+uint8_t Buzzer_OUTGET(void);//读取蜂鸣器状态
+
+#endif
