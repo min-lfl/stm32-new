f97f3b886a1cff541b47160b4d7f277b95ffd687
min-lfl <2775381313@qq.com>
2025-11-30 20:39:20 +0800

    完成定时器中断的初始化

---
+++ 项目8-定时器定时中断/User/main.c
@@ -0,0 +1,21 @@
+#include "stm32f10x.h"                  // Device header
+#include <stdio.h>
+#include <DELAY.H>
+#include <LED.H>
+#include <KEY.H>
+#include <LightSensor.H>
+#include <Buzzer.H>
+#include <OLED.H>
+#include <CountSensor.H>
+#include <Encoder.H>
+
+
+
+int main(void){
+	OLED_Init();
+	OLED_ShowString(1,1,"num:");
+	while(1){
+		
+	}
+}
+
